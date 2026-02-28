bab9ce852d7400aad0993e570eb74d37ef1c7f66
Abdullah <66182508+zahinabrer5@users.noreply.github.com>
2026-02-28 14:05:47 -0500

    Initial commit

---
+++ src/main/java/org/uncreatives/e_hotels/EHotelsApplication.java
@@ -0,0 +1,13 @@
+package org.uncreatives.e_hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uncreatives/e_hotels/EHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uncreatives.e_hotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
